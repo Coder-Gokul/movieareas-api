7b32ae7852ad6b694f9b6f8b7263b428e67e9106
GOKULRAJ P <gokulrajmaster@gmail.com>
2024-06-11 22:38:52 +0530

    route structure

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ app.listen(PORT, ()=>{
 })
 
 //To kill the port which is already in use -> npx kill-port 6969 //
+
+// CRUD functionality of movies
+
+// R - For Reading movies
+app.get('/movies',(req,res) => {
+
+})
+
+// C - For Creating movies
+app.post('/movies',(req,res) => {
+
+})
+
+// U - For Updating movie
+app.get('/movies/:id',(req,res) => {
+
+})
+
+// D - For Deleting movie
+app.get('/movies/:id',(req,res) => {
+
+})
